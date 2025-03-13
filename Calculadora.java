c3f914b0cff1273ac513f54047a0f43fe865a98c
Rubens F. de Vilhena <rubens.vilhena22@gmail.com>
2025-03-13 15:35:12 -0300

    Update Calculadora.java

--- Calculadora.java
+++ Calculadora.java
@@ -1,47 +1,133 @@
 import java.util.Scanner;
+import java.util.*;
 
 public class Calculadora {
-
-    public static float sum(float a, float b) {return a + b;}
-
-    public static float subtraction(float a, float b) {return a - b;}
-
-    public static float multiplication(float a, float b) {return a * b;}
-
-    public static float division(float a, float b) {return a / b;}
-
+    //Funções de acordo com cada tipo de operação
+    //Soma
+    //Soma os valores do input até o usuário digitar 0
+    public static float sum(Scanner scanner){
+        float resultSum = 0;
+        for (;;){
+            System.out.println("Digite um valor para soma: ");
+            System.out.println("Quando quiser parar, digite 0");
+            float aux = readFloat(scanner);
+            if(aux == 0) return resultSum;
+            else {
+                resultSum += aux;
+            }
+        }
+    }
+    //Subtração
+    //O primeiro valor digitado pelo usuário será decrescido dos seguintes
+    //Subtrai os valores do input até o usuário digitar 0
+    public static float subtraction(Scanner scanner) {
+        float resultSubtraction = 0;
+        for (; ; ) {
+            System.out.println("Digite um valor para subtração:");
+            System.out.println("Quando quiser parar, digite 0");
+            float aux = readFloat(scanner);
+            if (aux == 0) return resultSubtraction;
+            else if (resultSubtraction == 0) resultSubtraction = aux;
+            else resultSubtraction -= aux;
+        }
+    }
+    //Multiplicação
+    //Multiplica os valores do input até o usuário digitar 0
+    public static float multiplication(Scanner scanner) {
+        float resultMultiplication = 0;
+        for (; ; ) {
+            System.out.println("Digite um valor para multiplicação:");
+            System.out.println("Quando quiser parar, digite 0");
+            float input = scanner.nextFloat();
+            if (input == 0) return resultMultiplication;
+            else if (resultMultiplication == 0) resultMultiplication = input;
+            else resultMultiplication *= input;
+        }
+    }
+    //Divisão
+    public static float division(Scanner scanner) {
+        System.out.println("Digite o valor do numerador: ");
+        float numerador = readFloat(scanner);
+        System.out.println("Digite o valor do denominador: ");
+        float denominador = readFloat(scanner);
+        return numerador / denominador;
+    }
+    //Exponecial
+    public static double expo(Scanner scanner) {
+        System.out.println("Digite o valor da base do expoente: ");
+        double base = readDouble(scanner);
+        System.out.println("Digite o valor do expoente: ");
+        double exponencial = readDouble(scanner);
+        return Math.pow(base, exponencial);
+    }
+    //Logarítmo
+    public static double log(Scanner scanner){
+        System.out.println("Digite o valor do logaritmando: ");
+        double logarimtmando = readDouble(scanner);
+        System.out.println("Digite o valor da base do logaritmo: ");
+        double base = readDouble(scanner);
+        return Math.log(logarimtmando) / Math.log(base);
+    }
+    //Função para imprimir a introdução ao programa
     public static void introdutcion() {
         System.out.println("BEM VINDO À CALCULADORA DESENVOLVIDA EM JAVA");
         System.out.println("Aqui você poderá resolver algumas operações matemáticas simples, conforme menu abaixo:");
     }
 
-    public static void menu() {
+    //Função para imprimir o menu de operações da calculadora
+    public static int menu() {
+        List<String> menu = Arrays.asList("0-Sair", "1-Soma", "2-Subtração", "3-Divisão", "4-Multiplicação", "5-Exponencial", "6-Logarítmo");
         System.out.println("Digite o número correspondente a operação que você quer fazer: ");
-        System.out.println("1-Soma\n2-Subtração\n3-Divisão\n4-Multiplicação\n0-Sair");
+        menu.forEach(System.out::println);
         System.out.print("Sua resposta: ");
+        return menu.toArray().length;
     }
 
-    public static boolean menuValidation(int x){
-        if (x < 0 || x > 4) {
-                System.out.println("Valor inválido! Digite um número entre 0 e 4");
-                return false;
-        } else return true;
-    }
-
-    public static int readInteger(Scanner scanner) {
+    public static float readFloat(Scanner scanner) {
         for (;;) {
             String linhaDigitada = scanner.next();
             try {
-                return Integer.parseInt(linhaDigitada);
+                return Float.parseFloat(linhaDigitada);
             } catch (NumberFormatException e) {
                 System.out.println("Valor inválido! Tente novamente.");
             }
         }
     }
 
-    public static void thanks() {System.out.print("Obrigado por usar a calculadora!\nAté a próxima!\nDesenvolvido por DevRubensV");}
+    public static double readDouble(Scanner scanner) {
+        for (;;) {
+            String linhaDigitada = scanner.next();
+            try {
+                return Double.parseDouble(linhaDigitada);
+            } catch (NumberFormatException e) {
+                System.out.println("Valor inválido! Tente novamente.");
+            }
+        }
+    }
 
-    public static boolean validString(Scanner scanner){
+    public static double operation(Scanner scanner, float lengthMenu){
+        for(; ; ){
+            float op = readFloat(scanner);
+            if (op < 0 || op > lengthMenu) {
+                System.out.println("Valor inválido! Digite um número entre 0 e " + (lengthMenu - 1));
+            } else {
+                if (op == 0) return op;
+                else if (op == 1) return sum(scanner);
+                else if (op == 2) return subtraction(scanner);
+                else if (op == 3) return division(scanner);
+                else if (op == 4) return multiplication(scanner);
+                else if (op == 5) return expo(scanner);
+                else if (op == 6) return log(scanner);
+            }
+        }
+    }
+
+    public static void thanks() {
+        System.out.print("Obrigado por usar a calculadora!\nAté a próxima!\nDesenvolvido por DevRubensV");
+        System.out.print("Desenvolvido por DevRubensV");
+    }
+
+    public static boolean repeat(Scanner scanner){
         for(;;){
             System.out.println("Voce quer fazer outra operação?");
             String x = scanner.next().toLowerCase();
@@ -52,6 +138,8 @@ public class Calculadora {
             } else System.out.println("Respota invalida! Responda com sim ou nao");
         }
     }
+
+    //Função para receber valores para operações que permitem multiplas entradas
 
     public static void main(String[] args){
 
@@ -65,44 +153,28 @@ public class Calculadora {
         introdutcion();
 
         //Looping para operar enquanto operating = verdadeiro
-        while (operating){
-            //Variável para sinalizar a operação que o usuário deseja
-            int options;
+        do{
 
-            //Exibe o menu de opeções da calculadora
+            //Exibe o menu de opções da calculadora
+            //A variavel n recebe o cumprimento do array menu
+            int n = menu();
+
             //Recebe e valida a escolha do usuário para que seja numero inteiro
             //Valida a opção do usuário referente ao Menu de operações
-            //Deve ser valor entre 0 e 4 (limites inclusos)
-            do {
-                menu();
-                options = readInteger(scanner);
-            } while (!(menuValidation(options)));
+            //Deve ser valor entre 0 e o cumprimento do array menu (variavel n)
+            double result = operation(scanner, n);
 
             //Opção de saida a partir do menu
-            if (options == 0) break;
-
-            //Solicita o primeiro número para operação
-            System.out.print("Digite o primeiro número para a operação: ");
-            float num1 = scanner.nextFloat();
-
-            //Solicita o segundo número para operação
-            System.out.print("Digite o segundo número para a operação: ");
-            float num2 = scanner.nextFloat();
-
-            //Realiza a operação, conforme opção do usuário
-            if (options == 1) System.out.println("O resultado da soma é: " + sum(num1, num2));
-            else if (options == 2) System.out.println("O resultado da subtração é: " + subtraction(num1, num2));
-            else if (options == 3) System.out.println("O resultado da divisão é: " + division(num1, num2));
-            else if (options == 4) System.out.println("O resultado da multiplicação é: " + multiplication(num1, num2));
+            if (result == 0) break;
+            else System.out.println("O resultado da operação é: "+ result);
 
             //Questiona se o usuario quer fazer outra operação
             //E valida sua resposta como sim ou nao
-            boolean aux = validString(scanner);
+            boolean aux = repeat(scanner);
 
             //Se resposta for nao, encerra o programa
             if (!aux) operating = false;
-
-        }
+        } while (operating);
         //encerra a função scanner
         scanner.close();
         //Exibe os agradecimentos
